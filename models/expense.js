2e7f6e415a2f8a884a12e81e73a94d48c22017a5
kaylubr <kylebreyes092005@gmail.com>
2025-11-18 18:32:09 +0800

    Add model for Expense document

---
+++ models/expense.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+const expenseSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: String,
+  amount: {
+    type: Number,
+    required: true,
+    default: 0
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  status: {
+    type: String,
+    default: 'Pending'
+  }
+}, { timestamps: true })
+
+expenseSchema.set('toJSON', {
+  transform: (doc, ret) => {
+    ret.id = ret._id
+    delete ret._id
+    delete ret.__V
+  }
+})
+
+const Expense = mongoose.model('Expense', expenseSchema)
+
+module.exports = Expense
